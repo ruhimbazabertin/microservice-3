249f3905371b6d68d71576f172ae0fa4ed1ad16a
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2022-04-22 18:42:24 +0200

    microservice-version-3

---
+++ spring-ajax/src/main/java/com/ajax/springajax/controller/ViewController.java
@@ -0,0 +1,12 @@
+package com.ajax.springajax.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
